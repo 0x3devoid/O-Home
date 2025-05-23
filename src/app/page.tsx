aeb3f6155bb063ce0e5e0f2866e7d188a1ea0144
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2025-05-24 00:09:26 +0100

    criterial - eligibilty

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client"
+import { useState } from "react";
 import Image from "next/image";
 import Navbar from './navbar'
-import { WalletIcon } from "lucide-react"
+import { CopyIcon } from "lucide-react"
 
 
 export default function Home() {
+  const [connected, setIsconnected] = useState(false)
   return (
     <>
       <Navbar />
@@ -25,7 +28,7 @@ export default function Home() {
             />
           </div>
 
-          <div className="p-8 bg-gray-300 bg-opacity-20 w-full rounded-2xl" >
+          <div className="p-8 bg-[#ffffff] bg-opacity-5 w-full rounded-2xl" >
             <h2 className="text-2xl font-extrabold lg:ml-4">Connect Wallet</h2>
 
             <p className="text-[12px] mt-2  lg:ml-4">
@@ -61,12 +64,79 @@ export default function Home() {
         </div>
 
 
+        {connected ? <>
+          <div className="content mt-10">
+            <div className="flex justify-between w-full gap-3 lg:flex-nowrap flex-wrap">
+              <div className="w-full">
+                <h2 className="text-xl font-extrabold">Wallet  Connected!!!!</h2>
+                <p className="mt-3 text-[10px]">
+                  Great news! We found your account eligible, and you’re eligible to claim your reward🎉🎉🎉.
+                </p>
+              </div>
+
+              <div className="w-full">
+                <p className="text-sm">Referral a Friend to earn more point</p>
+                <div className="border border-[#19EF9D] rounded-3xl p-2 flex justify-between mt-3">
+                  <p className="text-[12px]">Capyuser/BellAsh2025.67261</p>
+
+                  <div className="rounded-3xl bg-[#006216] p-1 px-3 flex justify-center gap-1 cursor-pointer">
+                    <p className="text-[10px]">Copy</p>
+                    <CopyIcon width={10} height={10} />
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="py-5 flex justify-between lg:flex-nowrap flex-wrap gap-3">
+            <div className="p-4 bg-[#ffffff] bg-opacity-5 w-full rounded-2xl" >
+
+              <h3 className="">Total $Capy To Claim :</h3>
+
+              <h1 className="mt-10 font-extrabold text-2xl">35,740</h1>
+
+
+              <p className="mt-10 text-[8px]">You'll need to sign 1 chunks of transactions.</p>
+
+            </div>
+            <div className="p-4 bg-[#ffffff] bg-opacity-5 w-full rounded-2xl" >
+              <h3>Eligibility:</h3>
+
+              <div className="text-[10px] mt-2">
+                <ol className="list-decimal list-inside">
+                  <li>  Conduct at least 2 distinct monthly transactions.
+                  </li>
+
+                  <li>Conduct at least 6 distinct monthly transactions.</li>
+                  <li>Conduct at least 9 distinct monthly transactions.</li>
+                  <li>Conduct more than 100 transactions.</li>
+                  <li className="text-gray-600">Conduct Transaction exceeding $10,000</li>
+                  <li className="text-gray-600">Conduct Transaction exceeding $50,000</li>
+                  <li className="text-gray-600">Conduct Transaction exceeding $100,000</li>
+
+                </ol>
+              </div>
+
+            </div>
+          </div>
+
+          <div className="text-center py-5">
+            <p>Countdown</p>
+            <div className="flex justify-center ">
+              <p className='border border-[#1EEDD8] rounded-2xl p-4 px-7 text-[10px]'>14Days: 06Hours: 10Mins: 78Secs </p>
+            </div>
+          </div>
+        </> : <div className="flex justify-center mt-10">
+          <button className='border border-[#1EEDD8] rounded-2xl p-2 px-7 text-sm' onClick={(() => setIsconnected(true))}>Connect Wallet</button>
+        </div>}
 
 
 
-        <div className="flex justify-center mt-10">
-          <button className='border border-[#1EEDD8] rounded-2xl p-2 px-7 text-sm'>Connect Wallet</button>
-        </div>
+
+
+
 
 
 
@@ -79,11 +149,11 @@ export default function Home() {
           <h2 className="text-2xl font-extrabold">$Capy</h2>
 
           <p className="mt-2 text-[10px] lg:text-sm">
-            Leveraging the power of decentralized finance (DeFi) to provide innovative liquidity opportunities for Base community.
+            Leveraging the power of decentralized finance (DeFi) to provide innovative liquidity opportunities for HyperEvm community.
           </p>
         </div>
 
-        <hr/>
+        <hr />
 
         <div className="flex justify-between mt-5">
           <p className="text-[8px]">Capy,  All Rights Reserved | Copyright © 2025</p>
@@ -95,22 +165,22 @@ export default function Home() {
               alt="Next.js logo"
               width={20}
               height={20}
-              
+
             />
-             <Image
+            <Image
               className="dark:invert"
               src="/images/youtube.png"
               alt="Next.js logo"
               width={20}
               height={20}
-              
+
             /> <Image
               className="dark:invert"
               src="/images/instagram.png"
               alt="Next.js logo"
               width={20}
               height={20}
-              
+
             />
           </div>
         </div>
