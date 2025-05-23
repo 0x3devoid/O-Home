9a1e66d8886b512a8a826cae0bdc03084d02de04
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2025-05-23 23:21:08 +0100

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,53 +73,49 @@ export default function Home() {
       </main>
 
 
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        {/* <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a> */}
+      <footer className="mt-20">
+
+        <div className="mb-5">
+          <h2 className="text-2xl font-extrabold">$Capy</h2>
+
+          <p className="mt-2 text-[10px] lg:text-sm">
+            Leveraging the power of decentralized finance (DeFi) to provide innovative liquidity opportunities for Base community.
+          </p>
+        </div>
+
+        <hr/>
+
+        <div className="flex justify-between mt-5">
+          <p className="text-[8px]">Capy,  All Rights Reserved | Copyright © 2025</p>
+
+          <div className="flex justify-center">
+            <Image
+              className="dark:invert"
+              src="/images/discord.png"
+              alt="Next.js logo"
+              width={20}
+              height={20}
+              
+            />
+             <Image
+              className="dark:invert"
+              src="/images/youtube.png"
+              alt="Next.js logo"
+              width={20}
+              height={20}
+              
+            /> <Image
+              className="dark:invert"
+              src="/images/instagram.png"
+              alt="Next.js logo"
+              width={20}
+              height={20}
+              
+            />
+          </div>
+        </div>
       </footer>
+
     </>
   );
 }
